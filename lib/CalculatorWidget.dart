eff89bfbee77360ace5815e832962ca3f7952827
mswift42 <martin.haesler@gmail.com>
2017-05-09 16:41:52 +0100

    Add ThemeData.

---
+++ lib/CalculatorWidget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CalculatorWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+
+    );
+  }
+}
+
+final ThemeData _kTheme = new ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.teal,
+  accentColor: Colors.redAccent,
+);
