e3c033e0c4abc1bcef4bc2ec80c856b75e1a49dd
mswift42 <martin.haesler@gmail.com>
2017-05-10 15:19:57 +0100

    Implement class calculatorbody.

--- lib/CalculatorWidget.dart
+++ lib/CalculatorWidget.dart
@@ -5,10 +5,33 @@ class CalculatorWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-
+      theme: _kTheme,
+      home: new CalculatorHome(),
     );
   }
 }
+
+class CalculatorHome extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("E-Juice Calculator"),
+      ),
+      body: new _CalculatorBody(),
+    );
+  }
+}
+
+class _CalculatorBody extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material();
+  }
+}
+
 
 final ThemeData _kTheme = new ThemeData(
   brightness: Brightness.light,
