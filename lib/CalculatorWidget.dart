c031cdf30266ae62dbc69ddab3be220758029195
mswift42 <martin.haesler@gmail.com>
2017-05-13 11:55:16 +0100

    Add NicotineStrengthWidget.

--- lib/CalculatorWidget.dart
+++ lib/CalculatorWidget.dart
@@ -27,18 +27,18 @@ class _CalculatorBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Material(
       child: new Column(children: <Widget>[
-        new _PGVGSliderWidget(),
+        new PGVGSliderWidget(),
       ]),
     );
   }
 }
 
-class _PGVGSliderWidget extends StatefulWidget {
+class PGVGSliderWidget extends StatefulWidget {
   @override
   _PGVGSliderState createState() => new _PGVGSliderState();
 }
 
-class _PGVGSliderState extends State<_PGVGSliderWidget> {
+class _PGVGSliderState extends State<PGVGSliderWidget> {
   double ratio = 0.5;
   @override
   Widget build(BuildContext context) {
@@ -47,20 +47,48 @@ class _PGVGSliderState extends State<_PGVGSliderWidget> {
         horizontal: 8.0,
         vertical: 2.0,
       ),
-      child: new Row(children: <Widget>[
-        new Slider(
-          onChanged: (double value) {
-            setState(() {
-              ratio = value;
-            });
-          },
-          label: "PG/VG Ratio",
-          value: ratio,
-          max: 100.00,
-          min: 0.0,
-          divisions: 20,
-        ),
-      ]),
+      child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new SliderLabel("PG"),
+            new Slider(
+              onChanged: (double value) {
+                setState(() {
+                  ratio = value;
+                });
+              },
+              label: "PG/VG Ratio",
+              value: ratio,
+              max: 100.00,
+              min: 0.0,
+              divisions: 20,
+            ),
+            new SliderLabel("VG"),
+          ]),
+    );
+  }
+}
+
+class SliderLabel extends StatelessWidget {
+  String label;
+
+  SliderLabel(this.label);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text(label);
+  }
+}
+
+class NicotineStrengthWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+
+      ]
     );
   }
 }
