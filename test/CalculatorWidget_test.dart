dae6cba5e6a2aa8738c3352d5e6e7afdda857140
mswift42 <martin.haesler@gmail.com>
2017-05-21 12:43:26 +0100

    Add class _RadioListTileState.

---
+++ test/CalculatorWidget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fjc/CalculatorWidget.dart';
+
+void main() {
+  testWidgets(
+      "PGVGSliderWidget is a slider widget", (WidgetTester tester) async {
+        var pgsl = new PGVGSliderWidget();
+  });
+}
